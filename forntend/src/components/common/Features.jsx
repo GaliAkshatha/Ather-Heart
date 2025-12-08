a22909cdd21ba265b20241cb1db9af4c3e895dc6
GaliAkshatha <156352345+GaliAkshatha@users.noreply.github.com>
2025-12-08 05:56:21 +0530

    Update Features.jsx

--- forntend/src/components/common/Features.jsx
+++ forntend/src/components/common/Features.jsx
@@ -14,7 +14,7 @@ export default function Features() {
     <section id="features" className="py-24 bg-gray-50">
       <div className="max-w-7xl mx-auto px-6">
 
-        <h2 className="text-3xl font-bold text-center mb-10">Why Anther Heart?</h2>
+        <h2 className="text-3xl font-bold text-center mb-10">Why Ather Heart?</h2>
 
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
